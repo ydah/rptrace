ba0aabc2a5664564041e443ea2950562380dbc66
ydah <t.yudai92@gmail.com>
2026-02-21 10:43:28 +0900

    Expand integration coverage for Tracee and strace

--- spec/integration/tracee_spec.rb
+++ spec/integration/tracee_spec.rb
@@ -35,6 +35,47 @@ RSpec.describe Ptrace::Tracee do
     tracee&.detach
   end
 
+  it "spawns and traces /bin/echo with arguments" do
+    tracee = nil
+
+    with_ptrace_permission do
+      tracee = described_class.spawn("/bin/echo", "hello")
+      tracee.cont
+      event = tracee.wait(flags: Ptrace::Constants::WALL)
+
+      expect(event.exited?).to be(true)
+      expect(event.exit_status).to eq(0)
+    end
+  ensure
+    tracee&.detach
+  end
+
+  it "can read registers and process memory while stopped" do
+    tracee = nil
+
+    with_ptrace_permission do
+      tracee = described_class.spawn("/bin/true")
+      regs = tracee.registers.read
+
+      pc_reg, sp_reg = case Ptrace::CStructs.arch
+                       when :x86_64 then %i[rip rsp]
+                       when :aarch64 then %i[pc sp]
+                       end
+
+      expect(regs).to include(pc_reg, sp_reg)
+      expect(regs.fetch(pc_reg)).to be_a(Integer)
+
+      bytes = tracee.memory.read(regs.fetch(sp_reg), 16)
+      expect(bytes.bytesize).to eq(16)
+
+      tracee.cont
+      event = tracee.wait(flags: Ptrace::Constants::WALL)
+      expect(event.exited?).to be(true)
+    end
+  ensure
+    tracee&.detach
+  end
+
   it "reports syscall stops with PTRACE_SYSCALL" do
     tracee = nil
 
@@ -88,6 +129,20 @@ RSpec.describe Ptrace::Tracee do
       Process.wait(child_pid) if child_pid
     rescue Errno::ECHILD
       nil
+    end
+  end
+
+  it "yields syscall enter/exit events via Ptrace.strace" do
+    with_ptrace_permission do
+      events = []
+
+      Ptrace.strace("/bin/true") do |event|
+        events << event
+      end
+
+      expect(events).not_to be_empty
+      expect(events.any?(&:enter?)).to be(true)
+      expect(events.any?(&:exit?)).to be(true)
     end
   end
 end
